246c6d190afb0b317d35afe9d68b6cda5782a6db
Staffan Olsson <solsson@gmail.com>
2015-02-10 18:43:52 +0100

    Uses default theme. No dependency to Bootstrap.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 require('!style!css!./themes/alertify.css');
-require('!style!css!./themes/alertify.bootstrap.css');
+require('!style!css!./themes/alertify.default.css');
 require('./dist/alertify');
 
 // With no changes to the original source we must export from the global object
