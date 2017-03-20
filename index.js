16e0e7fb97b9be21ce3621540f0eb95fc2515211
Staffan Olsson <staffan@repos.se>
2017-03-20 11:13:36 +0100

    Webpack 2 says: It's no longer allowed to omit the '-loader' suffix when using loaders

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
-require('!style!css!./dist/themes/alertify.css');
-require('!style!css!./dist/themes/alertify.default.css');
+require('!style-loader!css-loader!./dist/themes/alertify.css');
+require('!style-loader!css-loader!./dist/themes/alertify.default.css');
 require('./dist/alertify');
 
 // With no changes to the original source we must export from the global object
